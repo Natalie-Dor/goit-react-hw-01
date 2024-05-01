a6285eb92815b1a658fe9745ff5988c46e6db28e
Natalie-Dor <varnata000@gmail.com>
2024-05-01 13:04:26 +0300

    friends list  - coloring status

--- src/components/FriendListItem/FriendListItem.jsx
+++ src/components/FriendListItem/FriendListItem.jsx
@@ -1,3 +1,4 @@
+import clsx from 'clsx';
 import css from './FriendListItem.module.css';
 export default function FriendListItem({
   friends: { avatar, name, isOnline },
@@ -6,7 +7,9 @@ export default function FriendListItem({
     <div className={css.item}>
       <img src={avatar} alt="Avatar" width="48" />
       <p>{name}</p>
-      <p>{isOnline ? 'Online' : 'Offline'}</p>
+      <p className={clsx(css.text, isOnline ? css.online : css.offline)}>
+        {isOnline ? 'Online' : 'Offline'}
+      </p>
     </div>
   );
 }
